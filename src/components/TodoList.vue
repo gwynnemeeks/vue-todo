f13c9f294b51919095b27e1abe7df98b3f20d675
Gwynne Meeks <meeks.gwynne@gmail.com>
2021-03-03 10:40:13 -0600

    wip attempt 2

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,18 +1,13 @@
 <template>
   <div>
     <h3>ToDo List</h3>
-    <div v-for="item in todos" :key="item.id">
-      <todo-item />
-    </div>
   </div>
 </template>
 
 <script>
 import { todoItems } from "../data";
-import TodoItem from "./TodoItem.vue";
 
 export default {
-  components: { TodoItem },
   data() {
     return {
       todos: [...todoItems]
